dbc6e0de55e8fc301118c9a5ec2dc50bae1a2328
ximenafarinae <ximenafarinae@gmail.com>
2024-11-01 11:20:51 -0300

    deleted unused files

--- src/utils/helpers.py
+++
@@ -1,4 +0,0 @@
-def save_model(np, model, filepath):
-    np.savez(filepath, w_hidden1=model.w_hidden1, b_hidden1=model.b_hidden1,
-             w_hidden2=model.w_hidden2, b_hidden2=model.b_hidden2,
-             w_output=model.w_output, b_output=model.b_output)
